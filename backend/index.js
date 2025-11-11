1fab99bb99c04ecb7f020ad872524272d00caf45
vikashgoswami30 <109417405+vikashgoswami30@users.noreply.github.com>
2025-11-12 01:42:01 +0530

    updated code and files

--- backend/index.js
+++ backend/index.js
@@ -11,17 +11,10 @@ import adminRoutes from './src/routes/admin.js';
 
 const app = express();
 app.use(cors());
-const allowedOrigins = ["https://assesment-vikash.vercel.app"];
 app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true); 
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = "The CORS policy for this site does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
+  origin: "https://assesment-vikash.vercel.app", 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials:true
 }));
 app.use(express.json());
 
